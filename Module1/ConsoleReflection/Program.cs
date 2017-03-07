13403682448b00e16b69348763ef1ea5341bac76
Thanh Thinh <thanhthinhcntt@gmail.com>
2017-03-07 21:18:58 +0700

    update

--- Module1/ConsoleReflection/Program.cs
+++ Module1/ConsoleReflection/Program.cs
@@ -13,7 +13,8 @@ namespace ConsoleReflection
         static void Main(string[] args)
         {
             Type[] oTypes;
-            string nameClass;
+     
+            string nameClass,strMethod;
             Assembly oAssembly;
             oAssembly = Assembly.LoadFrom("D:\\LTHDT\\Advanced-.Net-Propgaming\\Module1\\RelectionDemo\\bin\\Debug\\RelectionDemo.dll");
             oTypes = oAssembly.GetTypes();
@@ -27,10 +28,41 @@ namespace ConsoleReflection
             nameClass = Console.ReadLine();
             object obj = oAssembly.CreateInstance(nameClass);
             // get name method
-            foreach (MethodInfo items in obj.GetType().GetMethods())
+            foreach (var items in obj.GetType().GetMethods())
             {
-                Console.WriteLine("Method" + items.Name);
+                Console.WriteLine("\n Name Method is :" + items.Name);
             }
+            Console.WriteLine("Input method :");
+            strMethod = Console.ReadLine();
+            var methodInfo = obj.GetType().GetMethod(strMethod);
+            if (methodInfo != null)
+            {
+                var param = methodInfo.GetParameters();
+                if (param.Length > 0)
+                {
+                    object[] objpara = new object[param.Length];
+                    for (int i = 0; i < param.Length; i++)
+                    {
+                        if (param[i].ParameterType == typeof(String))
+                        {
+                            Console.WriteLine("input parameter type string");
+                            string parstring = Console.ReadLine();
+                            objpara[i++] = parstring;
+                        }
+                        else if(param[i].ParameterType == typeof(Int32))
+                        {
+                            Console.WriteLine("input parameter type int");
+                            int k;
+                            string s;
+                            s = Console.ReadLine();
+                            k = Int32.Parse(s);
+                            objpara[i++] = k;
+                        }
+
+                    }
+                }
+            }
+
             Console.ReadLine();
 
         }
