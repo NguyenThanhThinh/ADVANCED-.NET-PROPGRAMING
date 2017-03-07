0e000fa569125379ef36317c7334e247da2bacbb
Thanh Thinh <thanhthinhcntt@gmail.com>
2017-03-07 20:50:44 +0700

    update

---
+++ Module1/ConsoleReflection/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleReflection
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Type[] oTypes;
+            string nameClass;
+            Assembly oAssembly;
+            oAssembly = Assembly.LoadFrom("D:\\LTHDT\\Advanced-.Net-Propgaming\\Module1\\RelectionDemo\\bin\\Debug\\RelectionDemo.dll");
+            oTypes = oAssembly.GetTypes();
+            foreach (Type item in oTypes)
+            {
+                Console.WriteLine("Class {0}", item.Name);
+                Console.WriteLine("FullName {0}", item.FullName);
+
+            }
+            Console.WriteLine("input Class:");
+            nameClass = Console.ReadLine();
+            object obj = oAssembly.CreateInstance(nameClass);
+            // get name method
+            foreach (MethodInfo items in obj.GetType().GetMethods())
+            {
+                Console.WriteLine("Method" + items.Name);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
--- Module1/RelectionDemo/Dog.cs
+++ Module1/RelectionDemo/Dog.cs
@@ -9,22 +9,22 @@ namespace RelectionDemo
     class Dog:Animal
     {
         private string _name;
-        public Dog()
-        {
-            _name = "Lulu";
-        }
-        public override void Cry()
-        {
-            Console.WriteLine("Woof");
-        }
+            public Dog()
+            {
+                _name = "Lulu";
+            }
+            public override void Cry()
+            {
+                Console.WriteLine("Woof");
+            }
 
-        public override void PrintName()
-        {
-            Console.WriteLine("My name is {0}. i am aDog.", _name);
-        }
-        public void SetName(string name)
-        {
-            this._name = name;
-        }
+            public override void PrintName()
+            {
+                Console.WriteLine("My name is {0}. i am aDog.", _name);
+            }
+            public void SetName(string name)
+            {
+                this._name = name;
+            }
     }
 }
