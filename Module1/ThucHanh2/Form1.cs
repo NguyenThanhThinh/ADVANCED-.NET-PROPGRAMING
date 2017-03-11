39d207bc8f155c792a438f79a21cd44d9df46609
Thanh Thinh <thanhthinhcntt@gmail.com>
2017-03-11 21:29:09 +0700

    update v2

--- Module1/ThucHanh2/Form1.cs
+++ Module1/ThucHanh2/Form1.cs
@@ -17,6 +17,8 @@ namespace ThucHanh2
         {
             InitializeComponent();
         }
+        public delegate void DelegateUpdateStatus(int i);
+        public DelegateUpdateStatus delUpdateStatus;
         private void BackupServer1NonMultiThreading()
         {
             int iSize;
@@ -36,12 +38,47 @@ namespace ThucHanh2
                     lblServer1.Refresh();
                     Thread.Sleep(TimeSpan.FromSeconds(0.01));
                 }
-
+            }
+            else
+            {
+             
+                btnStart1.Text = "Start";
+            }
+        }
+        public void BackupServer1Thread()
+        {
+            int iSize;
+            float percenTage;
+            Random rd;
+            if (btnStart1.Text.Equals("Start"))
+            {
+                btnStart1.Text = "Stop";
+                btnStart1.Refresh();
+                rd = new Random();
+                iSize = rd.Next(500, 1000);
+                for (int i = 0; i <= iSize; i++)
+                {
+                
+                    percenTage = (((float)i / (float)iSize) * 100);
+                    delUpdateStatus = new DelegateUpdateStatus(updateStatus);
+                    progressBarServer1.BeginInvoke(delUpdateStatus, new object[]
+                    {
+                        (int)percenTage
+                    });
+                    //progressBarServer1.Value = (int)percenTage;
+                    lblServer1.Text = (int)percenTage + "%";
+                    lblServer1.Refresh();
+                    Thread.Sleep(TimeSpan.FromSeconds(0.01));
+                }
             }
             else
             {
                 btnStart1.Text = "Start";
             }
+        }
+        public void updateStatus(int i)
+        {
+            progressBarServer1.Value = i;
         }
         private void button1_Click(object sender, EventArgs e)
         {
@@ -51,7 +88,9 @@ namespace ThucHanh2
 
         private void btnStart1_Click(object sender, EventArgs e)
         {
-            BackupServer1NonMultiThreading();
+            //BackupServer1NonMultiThreading();
+            BackupServer1Thread();
+
 
         }
 
