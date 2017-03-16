b54f019b508b4d44df414deda6634eb5699a74bf
Thanh Thinh <thanhthinhcntt@gmail.com>
2017-03-16 21:20:40 +0700

    add method dataprovider

--- Module1/ThucHanh3.DAL/DataProvider.cs
+++ Module1/ThucHanh3.DAL/DataProvider.cs
@@ -8,9 +8,122 @@ using System.Threading.Tasks;
 
 namespace ThucHanh3.DAL
 {
-    public class DataProvider
+    public class Dataprovider:IDataprovider
     {
+        private SqlConnection _connection;
+        private string _database= "atmsproject";
+        private string _password;
+        private string _server= @".\SQLExpress";
+        private string _user;
+        private static bool blnSecurity=false;
+    
+        public void Close()
+        {
+            _connection.Close();
+        }
 
+        public void ConnectionDb()
+        {
+            string connection;
+            if (blnSecurity == false)
+            {
+                connection = "Server" + _server + ";Database=" + _database + ";Initial Catalog =" + blnSecurity.ToString() + ";UID=" + _user + ";PWD=" + _password;
+                _connection = new SqlConnection(connection);
+                _connection.Open();
+            }
+            else
+            {
+                connection = "Server" + _server + ";Database=" + _database + ";Initial Catalog =" + blnSecurity.ToString();
+                _connection = new SqlConnection(connection);
+                _connection.Open();
+            }
+         
 
+        }
+
+        public void DBConnection()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DBConnection(bool statusConnection)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            _connection.Dispose();
+        }
+
+        public SqlDataReader ExecuteReader(string sql)
+        {
+            throw new NotImplementedException();
+        }
+
+        public SqlDataReader ExecuteReader(SqlCommand sqlCommand)
+        {
+            throw new NotImplementedException();
+        }
+        
+        public SqlDataReader ExecuteReader(string sql, SqlParameter[] parm)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int ExecuteScalar(SqlCommand sqlCommand)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int ExecuteScalar(string sql, SqlParameter[] parm)
+        {
+            throw new NotImplementedException();
+        }
+
+        public SqlConnection GetConnection()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetConnectionString()
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataSet GetDatasetResult(string sql)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataSet GetDatasetResult(SqlCommand sqlCommand)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataSet GetDatasetResult(string sql, string tableName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataSet GetDatasetResult(string sql, SqlParameter[] parm)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataSet GetDatasetResult(SqlCommand sqlCommand, string dataTable)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataSet GetDatasetResult(string sql, SqlParameter[] parm, string tableName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetTrustedConnectionString()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
---
+++ Module1/ThucHanh3.DAL/IDataprovider.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThucHanh3.DAL
+{
+    public interface IDataprovider: IDisposable
+    {
+        void Close();
+        void ConnectionDb();
+        void DBConnection();
+        void DBConnection(bool statusConnection);
+        SqlDataReader ExecuteReader(SqlCommand sqlCommand);
+        SqlDataReader ExecuteReader(string sql);
+        SqlDataReader ExecuteReader(string sql, SqlParameter[] parm);
+        int ExecuteScalar(SqlCommand sqlCommand);
+        int ExecuteScalar(string sql, SqlParameter[] parm);
+        SqlConnection GetConnection();
+        string GetConnectionString();
+        DataSet GetDatasetResult(SqlCommand sqlCommand);
+        DataSet GetDatasetResult(SqlCommand sqlCommand,string dataTable);
+        DataSet GetDatasetResult(string sql);
+        DataSet GetDatasetResult(string sql, SqlParameter[] parm);
+        DataSet GetDatasetResult(string sql, SqlParameter[] parm,string tableName);
+        DataSet GetDatasetResult(string sql,string tableName);
+        string GetTrustedConnectionString();
+
+
+
+
+    }
+}
